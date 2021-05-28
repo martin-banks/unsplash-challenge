00ab7b3dd955a030dd11b0c87f13f7777cb3452a
martin-banks <anotherbanksy@gmail.com>
2021-05-28 19:11:13 +1000

    adds redirect banner

--- components/update-banner/index.js
+++ components/update-banner/index.js
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 export default function UpdateBanner () {
 
   return <>
-    <a className={ styles.banner } href="">
+    <a className={ styles.banner } href="https://unsplash-challenge-update.martinbanks.com.au">
       <p>This is the original challenge version. Click here for an updated version.</p>
     </a>
   </>
