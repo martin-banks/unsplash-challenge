9e8e956fb3de76a6d353aaa14724de43ad7da8b7
martin-banks <anotherbanksy@gmail.com>
2021-05-31 12:38:40 +1000

    adds header component (unused)

---
+++ components/header/index.js
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+
+
+export default function Header () {
+  return <>
+    <div className={styles.container}>
+      <h1>Unsplash image challenge</h1>
+    </div>
+  </>
+}
